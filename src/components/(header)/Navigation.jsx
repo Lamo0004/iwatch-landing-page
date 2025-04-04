09ae288a6e1ec44de71cee67b7eb1d945b9df3c2
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-04-04 12:19:55 +0200

    undersider til alle punkter i navigationen

--- src/components/(header)/Navigation.jsx
+++ src/components/(header)/Navigation.jsx
@@ -9,13 +9,19 @@ const Navigation = () => {
     <nav>
       <ul className="flex gap-16 text-xl font-semibold">
         <li>
-          <a href="#"> Mac</a>
+          <Link href="imac" className={pathname === "/imac" ? "bg-white rounded-full text-[#b6ccda] py-2 px-8" : ""}>
+            iMac
+          </Link>
         </li>
         <li>
-          <a href="#"> iPhone</a>
+          <Link href="iphone" className={pathname === "/iphone" ? "bg-white rounded-full text-[#b6ccda] py-2 px-8" : ""}>
+            iPhone
+          </Link>
         </li>
         <li>
-          <a href="#">iPad</a>
+          <Link href="/ipad" className={pathname === "/ipad" ? "bg-white rounded-full text-[#b6ccda] py-2 px-4" : ""}>
+            iPad
+          </Link>
         </li>
         <li>
           <Link href="/iwatch" className={pathname === "/iwatch" ? "bg-white rounded-full text-[#b6ccda] py-2 px-8" : ""}>
